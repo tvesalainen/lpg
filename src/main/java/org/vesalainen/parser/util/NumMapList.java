35907770268b62ebcc28a337d4cd20febeb91def
Timo Vesalainen <timo.vesalainen@iki.fi>
2017-09-17 12:26:53 -0400

    Implemented getComparator method

--- src/main/java/org/vesalainen/parser/util/NumMapList.java
+++ src/main/java/org/vesalainen/parser/util/NumMapList.java
@@ -130,4 +130,10 @@ public class NumMapList<M extends Numerable,L> extends NumMap<M,List<L>> impleme
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public Comparator<L> getComparator()
+    {
+        return comparator;
+    }
+
 }
